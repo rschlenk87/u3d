90f2849bfe4d6387771b880a4690326009e19a4e
Robert Schlenker <iphoners87@web.de>
2018-08-02 12:06:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-rm -f /app/u3d/u3d*.gem
+rm -f u3d*.gem
 if [[ ! `gem list | grep "^bundler "` ]]; then gem install bundler; fi
 bundle install
-gem build /app/u3d/u3d.gemspec
-gem install --http-proxy http://ebeu5jt:toomuchdonnerstag@10.2.20.158:3128/ /app/u3d/*.gem
+gem build u3d.gemspec
+gem install --http-proxy http://ebeu5jt:toomuchdonnerstag@10.2.20.158:3128/ *.gem
